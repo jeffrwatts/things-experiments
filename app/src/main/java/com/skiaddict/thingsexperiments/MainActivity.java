3341a4e12ecf1669fe986ed8f2672cdf696e18cf
jeffrwatts <jeffrwatts@gmail.com>
2017-08-06 13:38:29 -0700

    Added GPS.

--- app/src/main/java/com/skiaddict/thingsexperiments/MainActivity.java
+++ app/src/main/java/com/skiaddict/thingsexperiments/MainActivity.java
@@ -1,16 +1,23 @@
 package com.skiaddict.thingsexperiments;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.things.contrib.driver.bmx280.Bmx280SensorDriver;
 import com.google.android.things.contrib.driver.button.Button;
+import com.google.android.things.contrib.driver.gps.NmeaGpsDriver;
+import com.google.android.things.contrib.driver.mma7660fc.Mma7660FcAccelerometerDriver;
 
 import java.io.IOException;
 import java.net.SocketException;
@@ -25,22 +32,42 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    // For Button
     private final String BUTTON_GPIO_PIN = "BCM21";
+
+    // For Temperature, Pressure, Accelerometer.
     private final String I2C1_PIN = "I2C1";
+
+    // For GPS
+    private final String UART_PIN = "UART0";
+    public static final int UART_BAUD = 9600;
+    public static final float GPS_ACCURACY = 2.5f; // From GPS datasheet
 
     private Button cameraButton;
     private DeviceCamera deviceCamera;
     private SensorManager sensorManager;
+    private LocationManager locationManager;
     private DynamicSensorCallback dynamicSensorCallback;
+
     private Bmx280SensorDriver bmx280SensorDriver;
+    private Mma7660FcAccelerometerDriver accelerometerDriver;
+    private NmeaGpsDriver gpsDriver;
+
     private TemperatureSensorEventListener temperatureSensorEventListener;
     private PressureSensorEventListener pressureSensorEventListener;
+    private AccelerometerSensorEventListener accelerometerSensorEventListener;
+
+    private GpsLocationListener gpsLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+
+        // Get instance of locatinManager
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         // Get instance of sensorManager and register a callback.
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -52,10 +79,35 @@ public class MainActivity extends Activity {
             bmx280SensorDriver = new Bmx280SensorDriver(I2C1_PIN);
             bmx280SensorDriver.registerTemperatureSensor();
             bmx280SensorDriver.registerPressureSensor();
+            Log.d(TAG, "Registered Bmx280SensorDriver");
         } catch (IOException e) {
             Log.d(TAG, "Unable to register Bmx280SensorDriver: " + e.getLocalizedMessage());
         }
 
+        // Set up Accelerometer Driver
+        try {
+            accelerometerDriver = new Mma7660FcAccelerometerDriver(I2C1_PIN);
+            accelerometerDriver.register();
+            Log.d(TAG, "Regisetred Mma7660FcAccelerometerDriver");
+
+        } catch (IOException e) {
+            Log.d(TAG, "Unable to register Mma7660FcAccelerometerDriver: " + e.getLocalizedMessage());
+        }
+
+        // Set up GPS Driver
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            try {
+                gpsDriver = new NmeaGpsDriver(this, UART_PIN, UART_BAUD, GPS_ACCURACY);
+                gpsDriver.register();
+                gpsLocationListener = new GpsLocationListener();
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, gpsLocationListener);
+                Log.d(TAG, "Regisetred NmeaGpsDriver");
+            } catch (IOException e) {
+                Log.d(TAG, "Unable to register NmeaGpsDriver: " + e.getLocalizedMessage());
+            }
+        }
+
+        // Setup GPIO Button to trigger camera (for now).
         try {
             cameraButton = new Button(BUTTON_GPIO_PIN, Button.LogicState.PRESSED_WHEN_LOW);
             cameraButton.setOnButtonEventListener(new Button.OnButtonEventListener() {
@@ -100,8 +152,29 @@ public class MainActivity extends Activity {
                 bmx280SensorDriver.close();
             }
         } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        // Unregister accelerometer.
+        if (null == accelerometerSensorEventListener) {
+            sensorManager.unregisterListener(accelerometerSensorEventListener);
+            accelerometerSensorEventListener = null;
+        }
+
+        try {
+            if (null != accelerometerDriver) {
+                accelerometerDriver.unregister();
+                accelerometerDriver.close();
+            }
+        } catch (IOException e) {
+        }
+
+            try {
+                if (null != gpsDriver) {
+                    gpsDriver.unregister();
+                    gpsDriver.close();
+                }
+            } catch (IOException e) {
+            }
 
         // Remove the sensor callback.
         sensorManager.unregisterDynamicSensorCallback(dynamicSensorCallback);
@@ -110,11 +183,11 @@ public class MainActivity extends Activity {
         try {
             cameraButton.close();
         } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
     private void takePicture() {
+        // Just testing Rx.
         Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> e) throws Exception {
@@ -199,6 +272,15 @@ public class MainActivity extends Activity {
                 pressureSensorEventListener = new PressureSensorEventListener();
                 sensorManager.registerListener(pressureSensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
+            } else if (sensorType == Sensor.TYPE_ACCELEROMETER) {
+                Log.d(TAG, "Accelerometer Sensor Connected: " + sensor.getName());
+                if (null != accelerometerSensorEventListener) {
+                    sensorManager.unregisterListener(accelerometerSensorEventListener);
+                    accelerometerSensorEventListener = null;
+                }
+                accelerometerSensorEventListener = new AccelerometerSensorEventListener();
+                sensorManager.registerListener(accelerometerSensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
             }
             else {
                 Log.d(TAG, "onDynamicSensorConnected called for sensor = : " + sensor.getName());
@@ -235,6 +317,42 @@ public class MainActivity extends Activity {
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
             Log.d(TAG, "Pressure onAccuracyChanged: accuracy - " + accuracy);
+        }
+    }
+
+    private class AccelerometerSensorEventListener implements SensorEventListener {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            Log.d(TAG, "Accelerometer onSensorChanged: Values = " + event.values[0]);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            Log.d(TAG, "Accelerometer onAccuracyChanged: accuracy - " + accuracy);
+        }
+    }
+
+    private class GpsLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d(TAG, "Location update: " + location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.d(TAG, "onStatusChanged.");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d(TAG, "onProviderEnabled.");
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d(TAG, "onProviderDisabled.");
         }
     }
 }
